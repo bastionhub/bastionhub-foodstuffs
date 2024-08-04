f614982bbe58ce80f15a408ac1f5ce38d571233e
bastionhub <qprakken@gmail.com>
2024-08-04 00:04:56 -0500

    Initial commit

---
+++ src/main/java/net/bastionhub/foodstuffs/FoodstuffsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bastionhub.foodstuffs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FoodstuffsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
